64ae4e373a77c4c9905f6acdeb7f54842bce42f5
Venita Kissell <venitak@hotmail.com>
2024-11-18 20:44:45 +1000

    feat: Example Dog class

---
+++ jsclasses/app.js
@@ -0,0 +1,18 @@
+class Dog {
+constructor(name, breed, age, color){
+
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+    this.color = color;
+}
+
+howl(){
+
+}
+
+}
+
+let brockie = new Dog('Brockie', 'German Shepherd', '3', 'Brown')
+
+console.log(brockie);
